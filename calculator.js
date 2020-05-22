955c83a41dc0e3e103fdf7e4c06b4d953c4e5f15
Eric Ramsay <64610054+eramsay20@users.noreply.github.com>
2020-05-22 16:50:48 -0400

    Initialize files to Master

---
+++ calculator.js
@@ -0,0 +1,24 @@
+/* Exercise Instructions:
+For this exercise, you will use HTML and JavaScript to construct a basic calculator. 
+The calculator should have two input boxes to submit operands. It should have a button 
+for each of the operations that will be performed. Clicking on one of these buttons will 
+perform the operation on the input in the input boxes.
+
+Requirements:
+
+1. All event listeners should be attached inside a window.onload() event.
+2. No global variables are used.
+3. Must have 2 input boxes for operands
+4. Must have a button for each operation
+5. Once an operation is clicked, display the results.
+6. Must allow for addition, subtraction, multiplication and division of two numbers.
+7. Should not allow division by zero. Display a message that says "Error: Cannot divide by 0"
+8. Must use functions to organize the program.
+
+Stretch Requirements:
+
+1. Provide input validation on the input fields
+2. Implement a memory feature where the result of a calculation can be stored and then
+recalled to use as input for another operation.
+3. Add the ability to raise a number to a power.
+4. Add the ability to calculate the square root of a number.*/
